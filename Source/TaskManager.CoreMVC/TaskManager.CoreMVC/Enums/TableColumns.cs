b20948ed616313844dffef8abe76563fb9e4032c
khaingmyat95 <khaingkhaingmyat1@gmail.com>
2023-03-07 21:29:36 +0630

    Project Source Code

---
+++ Source/TaskManager.CoreMVC/TaskManager.CoreMVC/Enums/TableColumns.cs
@@ -0,0 +1,41 @@
+﻿namespace TaskManager.CoreMVC.Enums
+{
+    public enum ToDoListTableColOrder
+    {
+        Status = 1,
+        TaskName,
+        Remark,
+        CancelReason,
+        IsPrority,
+        AssignDate,
+        EstimatedEndDate,
+        AssignEmployeeName,
+        UpdateDate,
+        UpdateEmployeeName
+    }
+
+    public enum EmpTaskTableColOrder
+    {
+        Status = 1,
+        EmpCode,
+        EmpName,
+        TaskName,
+        Remark,
+        CancelReason,
+        IsPrority,
+        AssignDate,
+        EstimatedEndDate,
+        AssignEmployeeName,
+        UpdateDate,
+        UpdateEmployeeName
+    }
+
+    public enum EmpTableColOrder
+    {
+        Code = 1,
+        Name,
+        Admin,
+        RegisteredDate,
+        UpdatedDate,
+    }
+}
